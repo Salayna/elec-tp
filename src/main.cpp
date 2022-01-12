83582ca15193f95ef95e8096d82c870b4e8aa1b0
Salayna <salaynadoukoure@gmail.com>
2022-01-12 14:57:09 +0100

    feat(src): Finished

--- src/main.cpp
+++ src/main.cpp
@@ -12,20 +12,36 @@ const int BUTTON_2 = A1;
 const int BUTTON_3 = A2;
 const int BUTTON_4 = A3;
 
+const int IN1_DIR = D4;
+const int IN1_PMW = D5;
+const int IN2_DIR = D6;
+const int IN2_PMW = D7;
+
 int button1State = LOW;
 int button2State = LOW;
 int button3State = LOW;
 int button4State = LOW;
 
 void setup (){
+  // Initializing The LEDs PinModes
  pinMode (LED_1,OUTPUT);
  pinMode (LED_2,OUTPUT);
  pinMode (LED_3,OUTPUT);
  pinMode (LED_4,OUTPUT);
+
+ // Initializing The buttons PinModes
  pinMode(BUTTON_1, INPUT);
  pinMode(BUTTON_2, INPUT);
  pinMode(BUTTON_3, INPUT);
  pinMode(BUTTON_4, INPUT);
+
+// Initializing The Motors pinmodes PinModes
+ pinMode(IN1_DIR, OUTPUT);
+ pinMode(IN1_PMW, OUTPUT);
+ pinMode(IN2_DIR, OUTPUT);
+ pinMode(IN2_PMW, OUTPUT);
+
+
  Serial.begin(9600);
   while (!Serial);
 
@@ -57,10 +73,15 @@ void loop(){
   if (IMU.gyroscopeAvailable()) {
     IMU.readGyroscope(x, y, z);
 
-    Serial.print(x);
+    Serial.printf("x:",x);
     Serial.print('\t');
     Serial.print(y);
     Serial.print('\t');
     Serial.println(z);
   }
+  digitalWrite(IN1_PMW, button1State);
+  digitalWrite(IN1_DIR, button2State);
+
+  digitalWrite(IN2_PMW, button3State);
+  digitalWrite(IN2_DIR, button4State);
 }
